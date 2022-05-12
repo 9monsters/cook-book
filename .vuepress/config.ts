2edd6739042b03f8ad06aed0646f94fa763e037b
wangjia <wangjia@patsnap.com>
2022-05-12 19:54:32 +0800

    update

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,12 +1,6 @@
 import { defineUserConfig } from 'vuepress'
 import { path } from '@vuepress/utils'
 
-import { hopeTheme } from 'vuepress-theme-hope'
-import { zhNavbarConfig } from './navBar';
-import { zhSidebarConfig } from './sideBar';
-
-import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
-import { containerPlugin } from '@vuepress/plugin-container'
 import { externalLinkIconPlugin } from '@vuepress/plugin-external-link-icon'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
@@ -14,59 +8,19 @@ import { nprogressPlugin } from '@vuepress/plugin-nprogress'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 
 import { searchPlugin } from '@vuepress/plugin-search'
-import { pwaPlugin } from '@vuepress/plugin-pwa'
-import { pwaPopupPlugin } from '@vuepress/plugin-pwa-popup'
-import { shikiPlugin } from '@vuepress/plugin-shiki'
 
 import { gitPlugin } from '@vuepress/plugin-git'
 import { tocPlugin } from '@vuepress/plugin-toc'
+
+import {temeeConfig} from './themeConfig'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'CookBook',
   description: 'CookBook ✨',
   public: 'public',
-  theme: hopeTheme({
-    repo: "NineSwordsMonster/cook-book",
-    repoLabel: "GitHub",
-    repoDisplay: true,
-    nav: zhNavbarConfig,
-    sidebar: zhSidebarConfig,
-    locales: {
-    },
-    breadcrumb: true,
-    displayFooter: true,
-    footer: 'MIT Licensed | Copyright © 2022-present wangjia',
-    copyright: 'WANGJIA',
-    git: {
-      timezone: "Asia/Shanghai",
-    },
-    mdEnhance: {
-      enableAll: true,
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
-    },
-    plugins: {
-      blog: {
-        autoExcerpt: true
-      },
-      comment: {
-        type: 'giscus',
-        repo: 'NineSwordsMonster/cook-book',
-        repoId: 'R_kgDOHUle5w',
-        category: 'Announcements',
-        categoryId: 'DIC_kwDOHUle584CPD56',
-        mapping: 'og:title',
-        reactionsEnabled: true,
-        inputPosition: 'top'
-      },
-    }
-  }),
-
+  theme: temeeConfig,
   plugins: [
-    backToTopPlugin(),
-    containerPlugin({type: 'tip'}),
     externalLinkIconPlugin({
     }),
     googleAnalyticsPlugin({
@@ -88,11 +42,6 @@ export default defineUserConfig({
       maxSuggestions: 6,
       isSearchable: (page) => page.path !== '/',
     }),
-    pwaPlugin({
-      serviceWorkerFilename: 'cookbook.service.worker.js'
-    }),
-    pwaPopupPlugin({}),
-    shikiPlugin({}),
     gitPlugin({
       createdTime: true,
       updatedTime: true,
--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 ---
 home: true
 ---
+
+```flow
+st=>start: 开始|past:>http://www.google.com[blank]
+e=>end: 结束|future:>http://www.google.com
+op1=>operation: 操作1|past
+op2=>operation: 操作2|current
+sub1=>subroutine: 子程序|invalid
+cond=>condition: 是/否?|approved:>http://www.google.com
+c2=>condition: 判断2|rejected
+io=>inputoutput: 进行反思...|future
+
+st->op1(right)->cond
+cond(yes, right)->c2
+cond(no)->sub1(left)->op1
+c2(yes)->io->e
+c2(no)->op2->e
+```
+
+```json
+{
+  "type": "bar",
+  "data": {
+    "labels": ["红色", "蓝色", "黄色", "绿色", "紫色", "橙色"],
+    "datasets": [
+      {
+        "label": "投票数",
+        "data": [12, 19, 3, 5, 2, 3],
+        "backgroundColor": [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        "borderColor": [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        "borderWidth": 1
+      }
+    ]
+  },
+  "options": {
+    "scales": {
+      "y": {
+        "beginAtZero": true
+      }
+    }
+  }
+}
+```
