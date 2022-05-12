6ae3b581ca98580f1ef60582053a159661feef72
wangjia <wangjia@patsnap.com>
2022-05-12 14:01:20 +0800

    fix public

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -21,6 +21,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: 'CookBook',
   description: 'CookBook',
+  public: path.resolve(__dirname, './public'),
   plugins: [
     backToTopPlugin(),
     containerPlugin({type: 'tip'}),
